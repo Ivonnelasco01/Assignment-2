a07ed76847d50e754d247d1b1c4d94f721a631fb
Ivonnelasco01 <92631714+Ivonnelasco01@users.noreply.github.com>
2021-10-28 01:50:48 +0800

    description

---
+++ program 2.py
@@ -0,0 +1,9 @@
+apple = int(input("How many apple will you buy?"))
+orange = int(input("How many orange will you buy?"))
+
+applePrc = apple * 20
+orangePrc = orange * 25
+
+total = applePrc + orangePrc
+
+print(f"The total amount is {total} pesos.")
